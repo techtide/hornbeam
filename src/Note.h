645df1246d57d689aed6be30bd2e2c15ee98b564
Arman Bhalla <armanbhalla6@gmail.com>
2024-02-16 14:56:52 -0600

    update

--- src/Note.h
+++ src/Note.h
@@ -8,31 +8,31 @@
 
 namespace hornbeam
 {
+    struct Embedding {
+
+    };
+
     /*
      * Data structure for storing notes/text-files in the Obsidian vault,
      * and representing them with their file-wide word/sentence embeddings.
-     * 
-     * To enable support for different word embedding models, each Note item
+     *
+     * To enable support for different word embedding models, each Note object
      * must implement its own computeEmbedding function, connecting to the
      * relevant static model.
      */
     class Note
     {
     public:
-        Note(const std::string &filePath) : filePath_(filePath) {
-            embedding_ = computeEmbedding();
-        }
+        Note(const std::string &filePath, const Embedding &embedding) : filePath_(filePath), embedding_(embedding) {}
 
         std::string getFilePath() const { return filePath_; }
-        FileEmbedding getEmbedding() const { return embedding_;}
+        Embedding getEmbedding() const { return embedding_; }
         int getGroupId() const;
 
     private:
         std::string filePath_;
-        FileEmbedding embedding_;
+        Embedding embedding_;
         int groupId_;
-
-        FileEmbedding computeEmbedding();
     };
 } // namespace hornbeam
 
